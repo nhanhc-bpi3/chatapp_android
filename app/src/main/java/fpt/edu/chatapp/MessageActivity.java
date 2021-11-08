3a4bf5289866f01819771c2e4b293858575c50b7
Magi <letuan79200@gmail.com>
2021-11-08 21:04:32 +0700

    Fix crash app

--- app/src/main/java/fpt/edu/chatapp/MessageActivity.java
+++ app/src/main/java/fpt/edu/chatapp/MessageActivity.java
@@ -82,7 +82,7 @@ public class MessageActivity extends AppCompatActivity {
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // and this
+
                 startActivity(new Intent(MessageActivity.this, MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
             }
         });
